ab42b16e3bf5985fea5cb5dd64e984bbc589d935
spikeburton <ralphburton3@gmail.com>
2020-05-11 09:29:15 -0400

    add shell scripts to generate certs and run registry

---
+++ share/certs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+mkdir -p certs
+openssl req -newkey rsa:4096 -nodes -sha256 \
+  -keyout certs/domain.key \
+  -x509 -days 365 \
+  -out certs/domain.crt
---
+++ share/registry.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker container run -d --name registry -p 5000:5000 --restart unless-stopped \
+  -v $(pwd)/registry-data:/var/lib/registry \
+  -v $(pwd)/certs:/certs \
+  -v $(pwd)/auth:/auth \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+  -e REGISTRY_AUTH=htpasswd \
+  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+  -e "REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd" \
+  registry
